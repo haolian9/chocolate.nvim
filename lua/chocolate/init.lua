a0b7508cf1dd52ba20119f7653f6a8e60b4f774c
haoliang <haoliang0.1.2@gmail.com>
2025-12-15 22:07:00 +0800

    sync with upstream

--- lua/chocolate/init.lua
+++ lua/chocolate/init.lua
@@ -1,8 +1,1 @@
----@class Chocolate
----@field vsel fun()
----@field cword fun()
----@field clear fun()
-
----@param flavor 'dove'|'snicker'
----@return Chocolate
-return function(flavor) return require(string.format("chocolate.%s", flavor)) end
+return require("chocolate.dove")
